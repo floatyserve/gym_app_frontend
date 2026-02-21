d0a44d1e526767f8d93e79171312f6388464b081
Vladyslav Dorosh <vladyslav.dorosh@student.tuke.sk>
2026-02-21 10:41:46 +0100

    modified locker.api.ts to match new backend

--- frontend/src/api/locker.api.ts
+++ frontend/src/api/locker.api.ts
@@ -2,16 +2,27 @@ import api from "./api.ts";
 import type {Locker} from "../types/locker/Locker.ts";
 import type {LockerStats} from "../types/locker/LockerStats.ts";
 import type {PageResponse} from "../types/api/PageResponse.ts";
+import type {LockerSearchRequest} from "../types/locker/LockerSearchRequest.ts";
+import type {CreateLockerRequest} from "../types/locker/CreateLockerRequest.ts";
 
 const baseURL = "/lockers"
 
-export async function getAll(): Promise<PageResponse<Locker>> {
-    return await api.get(baseURL)
+export async function searchLockers(
+    params: LockerSearchRequest
+): Promise<PageResponse<Locker>> {
+    return await api.get(baseURL, {params})
         .then(res => res.data);
 }
 
 export async function getStats(): Promise<LockerStats> {
     return await api.get(`${baseURL}/stats`)
+        .then(res => res.data);
+}
+
+export async function createLocker(
+    request: CreateLockerRequest
+): Promise<Locker> {
+    return await api.post(baseURL, request)
         .then(res => res.data);
 }
 
