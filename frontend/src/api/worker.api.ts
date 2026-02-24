c18f2779fc6c495f162ee40e9213412b98ecdc71
Vladyslav Dorosh <vladyslav.dorosh@student.tuke.sk>
2026-02-24 18:57:48 +0100

    expand api file

--- frontend/src/api/worker.api.ts
+++ frontend/src/api/worker.api.ts
@@ -2,11 +2,17 @@ import api from "./api.ts";
 import type {DetailedWorkerInfo, SimpleWorkerInfo} from "../types/worker/Worker.ts";
 import type {OnboardWorkerRequest} from "../types/worker/OnboardWorkerRequest.ts";
 import type {PageResponse} from "../types/api/PageResponse.ts";
+import type {UpdateWorkerRequest} from "../types/worker/UpdateWorkerRequest.ts";
 
 const baseURL = "/workers"
 
 export async function getSelf(): Promise<DetailedWorkerInfo> {
     return await api.get(`${baseURL}/me`)
+        .then(res => res.data);
+}
+
+export async function getWorkerById(id: number): Promise<DetailedWorkerInfo> {
+    return await api.get(`${baseURL}/${id}`)
         .then(res => res.data);
 }
 
@@ -19,5 +25,13 @@ export async function getAllWorkers(
 
 export async function onboardWorker(request: OnboardWorkerRequest): Promise<SimpleWorkerInfo> {
     return await api.post(`${baseURL}`, request)
+        .then(res => res.data);
+}
+
+export async function updateWorker(
+    id: number,
+    request: UpdateWorkerRequest
+): Promise<DetailedWorkerInfo> {
+    return await api.put(`/workers/${id}`, request)
         .then(res => res.data);
 }
